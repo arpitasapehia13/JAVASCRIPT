7ed70915722d49690e05a290ca96be52200d65ae
arpitasapehia13 <arpitasapehia@gmail.com>
2024-12-01 20:20:59 +0530

    Updated

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "33abs"
+console.log(typeof score)
+
+
+// CONVERSION
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// ture => 1, false => 0
+
+
+let isloggedIn = 1;
+let booleanisLoggedIn = Boolean(isloggedIn)
+console.log(booleanisLoggedIn)
+
+// "" => false
+// "somerandomText" => true
+
