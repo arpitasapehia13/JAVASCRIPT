c75897747f818e87a5b2e14f9314c076ee197b43
arpitasapehia13 <arpitasapehia@gmail.com>
2024-12-01 19:24:28 +0530

    Added files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountID = 145567
+let accountEmail = "arpita@gmail.com"
+var accountPassword = "12345"
+accountCity = "Punjab"
+
+// accountID = 2 -> this is not allowed
+accountEmail = "github@gmail.com"
+accountPassword = "23"
+accountCity = "Hyd"
+
+console.log(accountID);
+console.table([accountEmail,accountID,accountPassword,accountCity])
+
+
+
+
+/*
+***************************************************************THEORY:**************************************************************************
+
+const -> can't be changed 
+let -> can be changed
+var -> can be changed but this have issue with BLOCK SCOPE & FUNCTIONAL SCOPE. PREFER NOT TO USE 'VAR'
+
+*/
