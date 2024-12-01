81947d3f8b0cb51b7322516fbf1d6de5705c4d46
arpitasapehia13 <arpitasapehia@gmail.com>
2024-12-01 20:37:56 +0530

    Updated

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -3,18 +3,13 @@ console.log(typeof score)
 
 
 // CONVERSION
-
-
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber)
 console.log(valueInNumber)
 
-
-
 // "33" => 33
 // "33abc" => NaN
 // ture => 1, false => 0
-
 
 let isloggedIn = 1;
 let booleanisLoggedIn = Boolean(isloggedIn)
@@ -22,4 +17,24 @@ console.log(booleanisLoggedIn)
 
 // "" => false
 // "somerandomText" => true
+
+
+//***************************************************************OPERATIONS**********************************************************************/
+
+let value = 1
+let negative = -value
+console.log(negative);
+
+let str1 = "hi"
+let str2 = " arpita"
+let srt3 = str1 + str2
+console.log(srt3);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
 
