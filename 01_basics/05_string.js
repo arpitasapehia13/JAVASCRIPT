4675bc15e2aa4aa58971da0ec40dd6f39be1c4f8
arpitasapehia13 <arpitasapehia@gmail.com>
2024-12-02 20:34:17 +0530

    Added Files

---
+++ 01_basics/05_string.js
@@ -0,0 +1,26 @@
+const name = "arpita"
+const repo = 10
+
+// console.log(name + repo);
+
+console.log(`Hello this is ${name} my repo count is ${repo}`)  //this is string interpolation
+
+const gameName = new String('templerun')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+console.log(gameName.substring(0,5));
+console.log(gameName.slice(-8,4));
+// console.log(gameName.trim());
+console.log(gameName.replace('e' , 'h'));
+
+
+
+
+
+
+
+
