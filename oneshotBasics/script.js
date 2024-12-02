6a7a1fada147bebfb64fe027605d47fb7cf84f85
arpitasapehia13 <arpitasapehia@gmail.com>
2024-12-02 20:34:17 +0530

    Added

---
+++ oneshotBasics/script.js
@@ -0,0 +1,24 @@
+//word vs keyword
+//var const let
+//hoisting -> variable and function are hoisted means that there declaration is moved on the top of code
+var a ;
+a = 12;
+console.log(a);
+
+//types in js  -> PRIMITIVE AND REFERENCES
+// primitives = 
+// refernces = [], {}, () 
+
+var a = [12,13]
+var b = a;
+b.pop()
+console.log(a);
+
+//conditionals - if else ifelse
+ 
+
+
+
+
+ 
+
