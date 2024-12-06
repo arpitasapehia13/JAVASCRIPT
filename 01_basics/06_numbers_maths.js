37efbe380bf8b6094f5b9a4199a6df57159ada0b
arpitasapehia13 <arpitasapehia@gmail.com>
2024-12-06 22:42:47 +0530

    Added Files

---
+++ 01_basics/06_numbers_maths.js
@@ -0,0 +1,20 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 112.8966
+console.log(otherNumber.toPrecision(3));
+
+const number = 1000000
+console.log(number.toLocaleString());
+console.log(number.toLocaleString('en-US'));
+  
+
+
+
+
+
