02cc41bab01fa1f7a69a278a6803d892686ecf4d
arpitasapehia13 <arpitasapehia@gmail.com>
2024-12-06 23:32:07 +0530

    Updated

--- 01_basics/06_numbers_maths.js
+++ 01_basics/06_numbers_maths.js
@@ -12,6 +12,31 @@ console.log(otherNumber.toPrecision(3));
 const number = 1000000
 console.log(number.toLocaleString());
 console.log(number.toLocaleString('en-US'));
+
+
+/***************************************************************************MATHS************************************************************* */
+
+console.log(Math); //to check more value go on inspect and check in console
+
+console.log(Math.abs(-4));  //Absolute
+console.log(Math.round(456.34));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(1,9,4,2));
+console.log(Math.max(1,9,4,2));
+
+console.log(Math.random());  //this value lies from 0 to 1
+console.log((Math.random()*10)+1); 
+
+
+const min = 10;
+const max = 20;
+console.log(Math.random() * (max - min + 1) + min); //IMPORTANT
+
+
+
+
+
   
 
 
