45cc1c48f54230b66d1aa1a2c0a575352a70ff0d
arpitasapehia13 <arpitasapehia@gmail.com>
2024-12-12 20:20:44 +0530

    Updated in 02_scopes.js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -10,10 +10,47 @@ if(true){
     console.log("inner" , num1);
     
 }
-
-
 console.log(num1);   
 // console.log(num2);
 // console.log(num3);
 
 
+function one(){
+    const userName = "Arpita"
+
+    function two(){
+        const course = "JS"
+        console.log(userName);
+        
+    }
+    // console.log(course);
+    two();
+}
+one();
+
+
+if(true){
+    const user = "arpi"
+    if(user === "arpi"){
+        const website = " insta"
+        console.log(user + website);
+        
+    }
+    // console.log(website);
+}
+// console.log(user);
+
+
+//****************************************************************************INTERESTING****************************************************************************************************** */
+console.log(addone(1));
+function addone(num){
+    return num + 1;
+}
+// addone(1);
+
+
+addTwo(13);
+const addTwo = function(num){
+    return num + 2;
+}
+// addTwo(13);  --->>> this would now give error (TBR)
