c2bcd1518f25764df3619983201e9cd0774ca94d
arpitasapehia13 <arpitasapehia@gmail.com>
2024-12-14 20:47:39 +0530

    Added File

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,27 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 10
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+        
+    case 2:
+        console.log("Feburary");
+        break;
+
+    case 3:
+        console.log("March");
+        break;
+        
+    default:
+        console.log("October");
+        break;
+}
